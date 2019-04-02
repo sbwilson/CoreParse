31796c0ed572df65c68ee7afbc5fa16190cf06bc
Simon Wilson <simon@net5.com.au>
2019-04-02 18:00:09 +1000

    Trying to get Carthage to work

--- CoreParse/CoreParse.h
+++ CoreParse/CoreParse.h
@@ -2,11 +2,11 @@
 //  CoreParse.h
 //  CoreParse
 //
-//  Created by Fedor Pudeyan on 16.11.16.
-//  Copyright © 2016 In The Beginning... All rights reserved.
+//  Created by Simon Wilson on 2/4/19.
+//  Copyright © 2019 In The Beginning. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Cocoa/Cocoa.h>
 
 //! Project version number for CoreParse.
 FOUNDATION_EXPORT double CoreParseVersionNumber;
@@ -15,37 +15,5 @@ FOUNDATION_EXPORT double CoreParseVersionNumber;
 FOUNDATION_EXPORT const unsigned char CoreParseVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CoreParse/PublicHeader.h>
-#import "CPTokeniser.h"
 
-#import "CPTokenStream.h"
 
-#import "CPTokenRecogniser.h"
-#import "CPKeywordRecogniser.h"
-#import "CPNumberRecogniser.h"
-#import "CPWhiteSpaceRecogniser.h"
-#import "CPIdentifierRecogniser.h"
-#import "CPQuotedRecogniser.h"
-#import "CPRegexpRecogniser.h"
-
-#import "CPToken.h"
-#import "CPErrorToken.h"
-#import "CPEOFToken.h"
-#import "CPKeywordToken.h"
-#import "CPNumberToken.h"
-#import "CPWhiteSpaceToken.h"
-#import "CPQuotedToken.h"
-#import "CPIdentifierToken.h"
-
-#import "CPGrammarSymbol.h"
-#import "CPGrammarSymbol.h"
-#import "CPRule.h"
-#import "CPGrammar.h"
-
-#import "CPRecoveryAction.h"
-
-#import "CPParser.h"
-#import "CPSLRParser.h"
-#import "CPLR1Parser.h"
-#import "CPLALR1Parser.h"
-
-#import "CPJSONParser.h"
